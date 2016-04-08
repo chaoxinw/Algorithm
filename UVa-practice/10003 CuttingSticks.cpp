e7b69ef1a60625ffeb4fa6f44a9a8d6c725129db
chaoxinw <wcx970303@163.com>
2016-04-08 22:37:57 +1000

    Update 10003 CuttingSticks.cpp

--- UVa-practice/10003 CuttingSticks.cpp
+++ UVa-practice/10003 CuttingSticks.cpp
@@ -3,44 +3,47 @@
 #include <iostream>
 using namespace std;
 
-int minf(int a, int b){
-    return a <= b ? a : b;
+int minf(int a, int b) {
+	return a <= b ? a : b;
 }
 
-void cutting_func(int stick_l){
-    //int stick_l;      // < 1000
-    int num_cut;      // < 50
-    cin >> num_cut;
-    
-    int n = num_cut + 2;
-    int arr[n];
-    int i = 0;
-    for(i = 1;i < n-1;i++){
-        cin >> arr[i];
-    }
-    arr[0] = 0;
-    arr[n-1] = stick_l;
-    
-    int cost[n][n];
-    int min = 0x3f3f3f3f;
-    
-    for(int d = 0;d < n;d++){          //first columns then rows
-        for(int a = 0;a < n;a++){
-            int b = a + d;
-            if(b >= n){break;}
-            min = 0x3f3f3f3f;
-            for(int c = a+1;c < b;c++){
-                min = minf(min, cost[a][c]+cost[c][b]);
-            }
-            if (b - a <= 1){
-                cost[a][b] = 0;
-            }
-            else {
-                cost[a][b] = min + arr[b] - arr[a];
-            }
-        }
-    }
-    cout << cost[0][n-1]<<endl;
+void cutting_func() {
+	int stick_l;      // < 1000
+	while (cin >> stick_l) {
+		if (stick_l == 0) { break; }
+		int num_cut;      // < 50
+		cin >> num_cut;
+
+		int n = num_cut + 2;
+		int arr[60];
+		int i = 0;
+		for (i = 1; i < n - 1; i++) {
+			cin >> arr[i];
+		}
+		arr[0] = 0;
+		arr[n - 1] = stick_l;
+
+		int cost[60][60];
+		int min = 0x3f3f3f3f;
+
+		for (int d = 0; d < n; d++) {          //first columns then rows
+			for (int a = 0; a < n; a++) {
+				int b = a + d;
+				if (b >= n) { break; }
+				min = 0x3f3f3f3f;
+				for (int c = a + 1; c < b; c++) {
+					min = minf(min, cost[a][c] + cost[c][b]);
+				}
+				if (b - a <= 1) {
+					cost[a][b] = 0;
+				}
+				else {
+					cost[a][b] = min + arr[b] - arr[a];
+				}
+			}
+		}
+		cout << "The minimum cutting is " << cost[0][n - 1] <<"."<< endl;
+	}
 }
 
 
@@ -60,10 +63,7 @@ void cutting_func(int stick_l){
 //}
 
 int main(void) {
-    int n;
-    while(cin >> n){
-        if (n == 0){break;}
-        cutting_func(n);
-    }
-    return 0;
+	cutting_func();
+
+	return 0;
 }
